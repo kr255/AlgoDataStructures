030f851543f1f8b167452a494a0434e053459ae9
kamranraza <kr255@njit.edu>
2018-07-17 16:49:12 -0400

    different implementation of IS

--- Algo/src/com/algo/java/RandomDoDelete.java
+++ Algo/src/com/algo/java/RandomDoDelete.java
@@ -52,13 +52,16 @@ public class RandomDoDelete {
 	}
 	public static void insertionSort(int arr[])
 	{
-		for(int i = 1; i<arr.length;i++)
+		for(int i = 0; i<arr.length;i++)
 		{
-			for(int j = i; j > 0 && (arr[j] < arr[j-1]); j--)
+			for(int j = i+1; j < arr.length; j++)
 			{
-				int temp = arr[j];
-				arr[j] = arr[j-1];
-				arr[j-1] = temp;
+				if(arr[j] < arr[i])
+				{
+					int temp = arr[j];
+					arr[j] = arr[i];
+					arr[i] = temp;
+				}
 			}
 		}
 	}
