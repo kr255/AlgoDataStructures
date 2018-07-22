d7ecf0572fbab7c470da5899d5df0ad8d935535b
kamranraza <kr255@njit.edu>
2018-07-22 15:06:04 -0400

    merge

---
+++ Algo/src/com/elementary/sort/java/MergeSort.java
@@ -0,0 +1,53 @@
+package com.elementary.sort.java;
+
+public class MergeSort {
+
+	private static Comparable aux[];
+	private static boolean less(Comparable v, Comparable w)
+	{
+		return v.compareTo(w) < 0;
+	}
+	public static void sort(Comparable a[])
+	{
+		int N = a.length;
+		aux = new Comparable[N];
+		sort(a, 0, N-1);
+	}
+	private static void sort(Comparable a[], int low, int high)
+	{
+		if(high <= low)
+			return;
+		int mid = low + (high - low) /2;
+		sort(a, low, mid);
+		sort(a, mid+1, high);
+		merge(a, low, mid, high);
+	}
+	private static void merge(Comparable a[], int low, int mid, int high)
+	{
+		int i = low, j = mid +1;
+		for(int k = low; k<=high; k++)
+		{
+			aux[k] = a[k];
+		}
+		for(int k = low; k<=high; k++)
+		{
+			if(i > mid)
+				a[k] = aux[j++];
+			else if (j > high)
+				a[k] = aux[i++];
+			else if(less(aux[j], aux[i]))
+				a[k] = aux[j++];
+			else
+				a[k] = aux[i++];
+		}
+	}
+	public static void main(String[] args) {
+		
+		Integer array[] = {2,5,9,4};
+		sort(array);
+		for(Integer i : array)
+		{
+			System.out.print(i + " ");
+		}
+	}
+}
