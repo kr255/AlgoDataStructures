2d0b2d723bcb2a63063ad470d5e693a807cffafc
kamranraza <kr255@njit.edu>
2018-08-04 17:24:09 -0400

    linear merge sort if array is sorted(not sure if its correct)

--- Algo/src/com/elementary/sort/java/MergeSort.java
+++ Algo/src/com/elementary/sort/java/MergeSort.java
@@ -20,7 +20,11 @@ public class MergeSort {
 		int mid = low + (high - low) /2;
 		sort(a, low, mid);
 		sort(a, mid+1, high);
-		merge(a, low, mid, high);
+		if(less(a[mid+1], a[mid]) || a[mid+1] == a[mid])
+		{
+			merge(a, low, mid, high);
+		}
+		//merge(a, low, mid, high);
 	}
 	private static void merge(Comparable a[], int low, int mid, int high)
 	{
@@ -43,9 +47,17 @@ public class MergeSort {
 	}
 	public static void main(String[] args) {
 		
-		Integer array[] = {2,5,9,4};
+//		Integer array[] = {2,5,9,4};
+//		sort(array);
+//		for(Integer i : array)
+//		{
+//			System.out.print(i + " ");
+//		}
+		//String array[] = {"q","u","e","s","t","i","o","n"};
+
+		String array[] = {"e","i","n","o","q","s","t","u"};
 		sort(array);
-		for(Integer i : array)
+		for(String i : array)
 		{
 			System.out.print(i + " ");
 		}
