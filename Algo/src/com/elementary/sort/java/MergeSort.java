3e54f5865f32ab05084e1590ddb74a6e43bfb806
kamranraza <kr255@njit.edu>
2018-08-04 20:46:53 -0400

    aux is not longer a static array

--- Algo/src/com/elementary/sort/java/MergeSort.java
+++ Algo/src/com/elementary/sort/java/MergeSort.java
@@ -2,31 +2,31 @@ package com.elementary.sort.java;
 
 public class MergeSort {
 
-	private static Comparable aux[];
+	//private static Comparable aux[];
 	private static boolean less(Comparable v, Comparable w)
 	{
 		return v.compareTo(w) < 0;
 	}
-	public static void sort(Comparable a[])
+	public static void sort(Comparable a[], Comparable aux[])
 	{
 		int N = a.length;
-		aux = new Comparable[N];
-		sort(a, 0, N-1);
+		//aux = new Comparable[N];
+		sort(a, aux, 0, N-1);
 	}
-	private static void sort(Comparable a[], int low, int high)
+	private static void sort(Comparable a[], Comparable aux[], int low, int high)
 	{
 		if(high <= low)
 			return;
 		int mid = low + (high - low) /2;
-		sort(a, low, mid);
-		sort(a, mid+1, high);
+		sort(a, aux, low, mid);
+		sort(a, aux, mid+1, high);
 		if(!less(a[mid+1], a[mid]) || a[mid] == a[mid+1])
 		{
 			return;
 		}
-		merge(a, low, mid, high);
+		merge(a, aux, low, mid, high);
 	}
-	private static void merge(Comparable a[], int low, int mid, int high)
+	private static void merge(Comparable a[], Comparable aux[], int low, int mid, int high)
 	{
 		int i = low, j = mid +1;
 		for(int k = low; k<=high; k++)
@@ -46,16 +46,11 @@ public class MergeSort {
 		}
 	}
 	public static void main(String[] args) {
-		
-//		Integer array[] = {2,5,9,4};
-//		sort(array);
-//		for(Integer i : array)
-//		{
-//			System.out.print(i + " ");
-//		}
-		//String array[] = {"q","u","e","s","t","i","o","n"};
-		String array[] = {"e","i","n","o","q","s","t","u"};
-		sort(array);
+				
+		String array[] = {"f","ex","ag","ss","yg","q","du","es","s","t","i","o","n"};
+		String aux[] = new String[array.length]; 
+		//String array[] = {"e","i","n","o","qx","s","t","u"};
+		sort(array, aux);
 		for(String i : array)
 		{
 			System.out.print(i + " ");
